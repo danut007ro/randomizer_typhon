87042ccffc467d993348646d5a08ac7690daac0f
Daniel Gorgan <danut007ro@gmail.com>
2022-06-06 22:51:01 +0300

    Update randomizer_typhon.lua

--- randomizer_typhon.lua
+++ randomizer_typhon.lua
@@ -12,245 +12,245 @@ local midi_channel
 local randomizing = false
 
 local modulation_ccs = {
-    {43, 59, 76}, -- CV LEVEL
-    {44, 60, 77}, -- CV1 LEVEL
-    {45, 61, 78}, -- CV2 LEVEL
-    {46, 62, 79}, -- WAVE LEVEL
-    {47, 63, 80}, -- CUT LEVEL
-    {48, 65, 81}, -- VCA LEVEL
-    {49, 66, 82}, -- FX1 LEVEL
-    {50, 67, 83}, -- FFM LEVEL
-    {51, 68, 84}, -- FX2 LEVEL
-    {52, 69, 85}, -- FX3 LEVEL
-    {53, 70, 86}, -- CUSTOM 1 LEVEL
-    {54, 71, 87}, -- CUSTOM 2 LEVEL
-    {55, 72, 88}, -- CUSTOM 3 LEVEL
+  {43, 59, 76}, -- CV LEVEL
+  {44, 60, 77}, -- CV1 LEVEL
+  {45, 61, 78}, -- CV2 LEVEL
+  {46, 62, 79}, -- WAVE LEVEL
+  {47, 63, 80}, -- CUT LEVEL
+  {48, 65, 81}, -- VCA LEVEL
+  {49, 66, 82}, -- FX1 LEVEL
+  {50, 67, 83}, -- FFM LEVEL
+  {51, 68, 84}, -- FX2 LEVEL
+  {52, 69, 85}, -- FX3 LEVEL
+  {53, 70, 86}, -- CUSTOM 1 LEVEL
+  {54, 71, 87}, -- CUSTOM 2 LEVEL
+  {55, 72, 88}, -- CUSTOM 3 LEVEL
 }
 
 function init()
-    params:add_separator("MIDI")
-    params:add_number("midi_device", "MIDI device", 1, 16, 1)
-    params:add_number("midi_channel", "MIDI channel", 1, 16, 1)
+  params:add_separator("MIDI")
+  params:add_number("midi_device", "MIDI device", 1, 16, 1)
+  params:add_number("midi_channel", "MIDI channel", 1, 16, 1)
 
-    params:add_separator("ADSR")
-    params:add_option("amp_adsr", "AMP ADSR", {"YES", "NO"}, 2)
-    params:add_option("vcf_adsr", "VCF ADSR", {"YES", "NO"}, 2)
+  params:add_separator("ADSR")
+  params:add_option("amp_adsr", "AMP ADSR", {"YES", "NO"}, 2)
+  params:add_option("vcf_adsr", "VCF ADSR", {"YES", "NO"}, 2)
 
-    params:add_separator("Modulators")
-    params:add_option("modulate_same_parameters", "Modulate same dest.", {"YES", "NO"}, 1)
-    params:add_option("m1_type", "M1 type", {"YES", "NO"}, 1)
-    params:add_number("m1_destinations", "M1 dest. count", 0, #modulation_ccs, 1)
-    params:add_number("m1_random_destinations", "M1 random dest. count", 0, #modulation_ccs, 3)
-    params:add_option("m2_type", "M2 type", {"YES", "NO"}, 1)
-    params:add_number("m2_destinations", "M2 dest. count", 0, #modulation_ccs, 1)
-    params:add_number("m2_random_destinations", "M2 random dest. count", 0, #modulation_ccs, 3)
-    params:add_option("m3_type", "M3 type", {"YES", "NO"}, 1)
-    params:add_number("m3_destinations", "M3 dest. count", 0, #modulation_ccs, 1)
-    params:add_number("m3_random_destinations", "M3 random dest. count", 0, #modulation_ccs, 3)
+  params:add_separator("Modulators")
+  params:add_option("modulate_same_parameters", "Modulate same dest.", {"YES", "NO"}, 1)
+  params:add_option("m1_type", "M1 type", {"YES", "NO"}, 1)
+  params:add_number("m1_destinations", "M1 dest. count", 0, #modulation_ccs, 1)
+  params:add_number("m1_random_destinations", "M1 random dest. count", 0, #modulation_ccs, 3)
+  params:add_option("m2_type", "M2 type", {"YES", "NO"}, 1)
+  params:add_number("m2_destinations", "M2 dest. count", 0, #modulation_ccs, 1)
+  params:add_number("m2_random_destinations", "M2 random dest. count", 0, #modulation_ccs, 3)
+  params:add_option("m3_type", "M3 type", {"YES", "NO"}, 1)
+  params:add_number("m3_destinations", "M3 dest. count", 0, #modulation_ccs, 1)
+  params:add_number("m3_random_destinations", "M3 random dest. count", 0, #modulation_ccs, 3)
 
-    params:add_separator("Set")
-    params:add_option("set_glide", "Glide amount", {"YES", "NO"}, 1)
-    params:add_option("set_vco", "VCO level", {"YES", "NO"}, 1)
-    params:add_option("set_trk", "Filter tracking", {"YES", "NO"}, 1)
-    params:add_option("set_ffm", "Filter FM amount", {"YES", "NO"}, 1)
+  params:add_separator("Set")
+  params:add_option("set_glide", "Glide amount", {"YES", "NO"}, 1)
+  params:add_option("set_vco", "VCO level", {"YES", "NO"}, 1)
+  params:add_option("set_trk", "Filter tracking", {"YES", "NO"}, 1)
+  params:add_option("set_ffm", "Filter FM amount", {"YES", "NO"}, 1)
 
-    params:add_separator("FX")
-    params:add_option("fx1_type", "FX1 type", {"YES", "NO"}, 1)
-    params:add_option("fx1_hpf", "FX1 HPF", {"YES", "NO"}, 1)
-    params:add_option("fx1_params", "FX1 parameters", {"YES", "NO"}, 1)
-    params:add_option("fx2_type", "FX2 type", {"YES", "NO"}, 1)
-    params:add_option("fx2_params", "FX2 parameters", {"YES", "NO"}, 1)
-    params:add_option("fx3_type", "FX3 type", {"YES", "NO"}, 1)
-    params:add_option("fx3_params", "FX3 parameters", {"YES", "NO"}, 1)
+  params:add_separator("FX")
+  params:add_option("fx1_type", "FX1 type", {"YES", "NO"}, 1)
+  params:add_option("fx1_hpf", "FX1 HPF", {"YES", "NO"}, 1)
+  params:add_option("fx1_params", "FX1 parameters", {"YES", "NO"}, 1)
+  params:add_option("fx2_type", "FX2 type", {"YES", "NO"}, 1)
+  params:add_option("fx2_params", "FX2 parameters", {"YES", "NO"}, 1)
+  params:add_option("fx3_type", "FX3 type", {"YES", "NO"}, 1)
+  params:add_option("fx3_params", "FX3 parameters", {"YES", "NO"}, 1)
 end
 
 function redraw()
-    screen.clear()
+  screen.clear()
 
-    screen.move(64, 15)
-    screen.level(15)
-    screen.text_center("Dreadbox Typhon randomizer")
+  screen.move(64, 15)
+  screen.level(15)
+  screen.text_center("Dreadbox Typhon randomizer")
 
-    if randomizing then
-        screen.move(64, 45)
-        screen.level(10)
-        screen.text_center("randomizing...")
-    end
+  if randomizing then
+    screen.move(64, 45)
+    screen.level(10)
+    screen.text_center("randomizing...")
+  end
 
-    screen.update()
+  screen.update()
 end
 
 function key(n, z)
-    if n == 2 and z == 0 and not randomizing then
-        randomizing = true
-        redraw()
-        randomize()
-        randomizing = false
-        redraw()
-    end
+  if n == 2 and z == 0 and not randomizing then
+    randomizing = true
+    redraw()
+    randomize()
+    randomizing = false
+    redraw()
+  end
 end
 
 function randomize_cc(cc, cc_min, cc_max)
-    cc_min = cc_min or 0
-    cc_max = cc_max or 127
-    if type(cc) ~= "table" then
-        cc = {cc}
-    end
+  cc_min = cc_min or 0
+  cc_max = cc_max or 127
+  if type(cc) ~= "table" then
+    cc = {cc}
+  end
 
-    for i=1,#cc do
-        midi_device:cc(cc[i], math.random(cc_min, cc_max), midi_channel)
-    end
+  for i=1,#cc do
+    midi_device:cc(cc[i], math.random(cc_min, cc_max), midi_channel)
+  end
 end
 
 function table.shallow_copy(original)
-    local copy = {}
-    for k,v in pairs(original) do
-        copy[k] = v
-    end
+  local copy = {}
+  for k,v in pairs(original) do
+    copy[k] = v
+  end
 
-    return copy
+  return copy
 end
 
 function sleep(s)
-    local ntime = os.clock() + s/10
-    repeat until os.clock() > ntime
+  local ntime = os.clock() + s/10
+  repeat until os.clock() > ntime
 end
 
 function randomize()
-    midi_device = midi.connect(params:get("midi_device"))
-    midi_channel = params:get("midi_channel")
+  midi_device = midi.connect(params:get("midi_device"))
+  midi_channel = params:get("midi_channel")
 
+  randomize_cc({
+    1, -- MOD WHEEL
+    2, -- CC2
+    3, -- RESONANCE
+    4, -- CUTOFF
+    5, -- WAVE
+    6, -- OSC
+  })
+  sleep(0.1)
+
+  if params:get("set_glide") == 1 then
     randomize_cc({
-      1, -- MOD WHEEL
-      2, -- CC2
-      3, -- RESONANCE
-      4, -- CUTOFF
-      5, -- WAVE
-      6, -- OSC
+      7, -- GLIDE AMOUNT
+    })
+  end
+
+  if params:get("set_vco") == 1 then
+    randomize_cc({
+      8, -- VCO LEVEL
+    })
+  end
+
+  if params:get("set_trk") == 1 then
+    randomize_cc({
+      9, -- FILTER TRACKING
+    })
+  end
+
+  if params:get("set_ffm") == 1 then
+    randomize_cc({
+      11, -- FILTER FM AMOUNT
+    })
+  end
+
+  sleep(0.1)
+
+  if params:get("amp_adsr") == 1 then
+    randomize_cc({
+      34, -- VCA EG ATTACK
+      35, -- VCA EG DECAY
+      36, -- VCA EG SUSTAIN
+      37, -- VCA EG RELEASE
+      38, -- VCA EG TIME
     })
     sleep(0.1)
+  end
 
-    if params:get("set_glide") == 1 then
-      randomize_cc({
-        7, -- GLIDE AMOUNT
-      })
-    end
-
-    if params:get("set_vco") == 1 then
-      randomize_cc({
-        8, -- VCO LEVEL
-      })
-    end
-
-    if params:get("set_trk") == 1 then
-      randomize_cc({
-        9, -- FILTER TRACKING
-      })
-    end
-
-    if params:get("set_ffm") == 1 then
-      randomize_cc({
-        11, -- FILTER FM AMOUNT
-      })
-    end
-
+  if params:get("vcf_adsr") == 1 then
+    randomize_cc({
+      27, -- VCF EG ATTACK
+      28, -- VCF EG DECAY
+      29, -- VCF EG SUSTAIN
+      30, -- VCF EG RELEASE
+      31, -- VCF EG TIME
+      33, -- VCF EG LEVEL
+    })
     sleep(0.1)
+  end
 
-    if params:get("amp_adsr") == 1 then
-      randomize_cc({
-        34, -- VCA EG ATTACK
-        35, -- VCA EG DECAY
-        36, -- VCA EG SUSTAIN
-        37, -- VCA EG RELEASE
-        38, -- VCA EG TIME
-      })
-      sleep(0.1)
-    end
-
-    if params:get("vcf_adsr") == 1 then
-      randomize_cc({
-        27, -- VCF EG ATTACK
-        28, -- VCF EG DECAY
-        29, -- VCF EG SUSTAIN
-        30, -- VCF EG RELEASE
-        31, -- VCF EG TIME
-        33, -- VCF EG LEVEL
-      })
-      sleep(0.1)
-    end
-
-    randomize_modulators()
-    randomize_fxs()
-    
-    if params:get("fx1_hpf") == 1 then
-      randomize_cc({
-        12, -- HP CUTOFF
-        13, -- HP RESONANCE
-      })
-    end
+  randomize_modulators()
+  randomize_fxs()
+  
+  if params:get("fx1_hpf") == 1 then
+    randomize_cc({
+      12, -- HP CUTOFF
+      13, -- HP RESONANCE
+    })
+  end
 end
 
 function randomize_modulators()
-    -- Table of allowed modulation parameters.
-    local parameters = table.shallow_copy(modulation_ccs)
+  -- Table of allowed modulation parameters.
+  local parameters = table.shallow_copy(modulation_ccs)
 
-    local index = 0
-    local modulators = {
-      ["m1"] = {["type"] = 93, ["cc"] = {40, 41, 42}},
-      ["m2"] = {["type"] = 94, ["cc"] = {56, 57, 58}},
-      ["m3"] = {["type"] = 95, ["cc"] = {73, 74, 75}},
-    }
-    for modulator, modulator_params in pairs(modulators) do
-      index = index + 1
+  local index = 0
+  local modulators = {
+    ["m1"] = {["type"] = 93, ["cc"] = {40, 41, 42}},
+    ["m2"] = {["type"] = 94, ["cc"] = {56, 57, 58}},
+    ["m3"] = {["type"] = 95, ["cc"] = {73, 74, 75}},
+  }
+  for modulator, modulator_params in pairs(modulators) do
+    index = index + 1
 
-      local destinations = params:get(modulator .. "_destinations")
-      local random_destinations = params:get(modulator .. "_random_destinations")
+    local destinations = params:get(modulator .. "_destinations")
+    local random_destinations = params:get(modulator .. "_random_destinations")
 
-      -- Randomize modulator type.
-      if params:get(modulator .. "_type") == 1 then
-        randomize_cc(modulator_params["type"], 0, 3)
+    -- Randomize modulator type.
+    if params:get(modulator .. "_type") == 1 then
+      randomize_cc(modulator_params["type"], 0, 3)
+    end
+
+    -- If no destinations should be modulated then leave as it is.
+    if destinations + random_destinations == 0 then
+      goto next_modulator
+    end
+
+    -- Randomize modulator parameters.
+    for param=1,#modulator_params["cc"] do
+      randomize_cc(modulator_params["cc"][param])
+    end
+
+    -- Update allowed parameters if allowed to modulate same.
+    if params:get("modulate_same_parameters") == 1 then
+      parameters = table.shallow_copy(modulation_ccs)
+    end
+
+    -- Reset modulator destinations.
+    for param=1,#parameters do
+      randomize_cc(parameters[param][index], 64, 64)
+    end
+    sleep(0.1)
+
+    for i=1,(destinations + random_destinations) do
+      -- Random destinations might be skipped.
+      if i > destinations and math.random(0, 1) == 0 then
+        goto next_destination
       end
 
-      -- If no destinations should be modulated then leave as it is.
-      if destinations + random_destinations == 0 then
-        goto next_modulator
-      end
-
-      -- Randomize modulator parameters.
-      for param=1,#modulator_params["cc"] do
-        randomize_cc(modulator_params["cc"][param])
-      end
-
-      -- Update allowed parameters if allowed to modulate same.
-      if params:get("modulate_same_parameters") == 1 then
-        parameters = table.shallow_copy(modulation_ccs)
-      end
-
-      -- Reset modulator destinations.
-      for param=1,#parameters do
-        randomize_cc(parameters[param][index], 64, 64)
+      -- Modulate parameter and remove from allowed.
+      if #parameters > 0 then
+        param = math.random(1, #parameters)
+        randomize_cc(parameters[param][index], 0, 127)
+        table.remove(parameters, param)
       end
       sleep(0.1)
 
-      for i=1,(destinations + random_destinations) do
-        -- Random destinations might be skipped.
-        if i > destinations and math.random(0, 1) == 0 then
-          goto next_destination
-        end
-
-        -- Modulate parameter and remove from allowed.
-        if #parameters > 0 then
-          param = math.random(1, #parameters)
-          randomize_cc(parameters[param][index], 0, 127)
-          table.remove(parameters, param)
-        end
-        sleep(0.1)
-
-        ::next_destination::
-      end
-
-      ::next_modulator::
+      ::next_destination::
     end
+
+    ::next_modulator::
+  end
 end
 
 function randomize_fxs()
